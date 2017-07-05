1c4c06a34c4cf74fd33bad3b13acf75898ea918d
Humberto <humberto1234shakha@hotmail.es>
2017-07-05 16:54:22 +0100

    creando cursos

--- collections/database.js
+++ collections/database.js
@@ -1,32 +1,129 @@
-CHAT = new  Mongo.Collection("chat");
-CONNECT = new Mongo.Collection("connect");
-var chatSchema = new SimpleSchema({
-	idSource: {
-		type:String
-	},
-	idDestination: {
-		type:String
-	},
-	date: {
+Images = new FilesCollection({
+  collectionName: 'Images',
+  allowClientCode: false, // Disallow remove files from g
+  storagePath:'/home/json/Documentos/pruevas/seminario 3.1/data',
+  onBeforeUpload: function (file) {
+    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+    if (file.size <= 11485760 && /png|jpg|jpeg|mp4|3gp/i.test(file.extension)) {
+      return true;
+    } else {
+      return 'Please upload image, with size equal or less than 10MB';
+    }
+  }
+});
+GALERIA = new Mongo.Collection('galeria',{
+    transform:function(item){
+        _.extend(item,{imagen:Images.findOne({_id:item.idImagen})});
+        return item;
+    }
+});
+var galeriaSchema = new SimpleSchema({
+    idUser: {
+        type : String,
+        autoValue : function(){
+            return Meteor.userId();
+        }
+    },
+    idImagen : {
+        type : String
+    }
+});
+GALERIA.attachSchema(galeriaSchema);
+
+
+
+//ANSWER
+Chat = new Mongo.Collection("chat");
+Answer = new Mongo.Collection("answer");
+Questions = new Mongo.Collection("questions");
+PERFIL = new Mongo.Collection("perfil");
+
+//Curso_User = new Mongo.Collection("cursos_user");
+
+//Curso_User = new Mongo.Collection"cursos_user"()
+Tomarcurso=new Mongo.Collection('tomarcurso')
+var tomar=new SimpleSchema({
+	fecha:{
 		type:Date
 	},
-	message: {
+	courseId:{
+		type:String
+	},
+	userId:{
 		type:String
 	}
 });
-CHAT.attachSchema(chatSchema);
-var connectSchema = new SimpleSchema({
-	idUs: {
+Tomarcurso.attachSchema(tomar);
+
+Cursos = new Mongo.Collection('cursos'/*,{
+	transform:function(item){
+		if(Accounts.user() != null && item.userId == Accounts.user()._id){
+			item.can_editar = true;
+		}else {
+			item.can_editar = false;
+		}
+		return item;
+	}
+}*/
+);
+
+
+Clases = new Mongo.Collection("clases");
+var clase=new SimpleSchema({
+	nombre:{
 		type:String
 	},
-	connectionDate: {
-		type:Date
+	detalle:{
+		type:String
 	},
-	disconnectionDate: {
-		type:Date
+	fecha:{
+		type:Date,
+		autoValue: function(){
+			return new Date();
+		}
 	},
-	stade: {
-		type:Boolean
+	archivo:{
+		type:String
+	},
+	chatId:{
+		type:String
+	},
+	cursId:{
+		type:String
 	}
 });
-CONNECT.attachSchema(connectSchema);
+Clases.attachSchema(clase);
+//Users = new Mongo.Collection("users");*/
+
+
+var cursoss = new SimpleSchema({
+	nombre: {
+		type:String
+	},
+	detalle: {
+		type:String
+	},
+	inicio: {
+		type:Date
+	},
+	fin: {
+		type:Date
+	},
+	userId: {
+		type:String
+	}/*,
+	clasesId: { 
+		type: [String]	
+	}
+	,
+	
+	clasesId : { 
+		type : Object , 
+		autoValue :function(){
+			return "";
+		}
+	}*/
+	//clases:[Clases]
+    
+});
+Cursos.attachSchema(cursoss);  
--- server/main.js
+++ server/main.js
@@ -1,6 +1,138 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+	
+	/*Meteor.publishComposite('listaCursos', function(idCurso){
+		return {
+			find(){
+				return Cursos.find();
+			},
+			children: [
+				{
+					find(curso){
+						return Clases.find({cursId:idCurso});
+					}
+				}
+			]
+		}
+	});*/
+	Meteor.publishComposite('listaClases', function(id){
+		return {
+			find(){
+				return Clases.find({cursId:id});
+			},
+			children: [
+				{
+					find(clases){
+						return Cursos.find({_id:clases.id});
+					}
+				}
+			]
+		}
+	});
+	/*Meteor.publishComposite('listaCursos', function(idCurso){
+		return {
+			find(){
+				return Cursos.find({_id:idCurso});
+			},
+			children: [
+				{
+					find(curso){
+						return Clases.find({cursId:idCurso});
+					}
+				}
+			]
+		}
+	});
+	*/
+	Meteor.methods({
+		/*"checkCourse" : function(obj) {
+			var l = Curso_User.find({iduser : obj.iduser,idcurso : obj.idcurso}).fetch();
+			if(l.length == 0){
+				return true;
+			}
+			return false;
+		},*/
+		"facicursos":function(id,msnObj){
+			Cursos.update({_id:id},{$set:{nombre:msnObj.nombre,detalle:msnObj.detalle}});
+			return true;
+		},
+		"deleteCurso": function(msnObj){
+			Cursos.remove(msnObj);
+			return true;
+		},
+		"addCourse": function(msnObj){
+			Cursos.insert(msnObj);
+			return true;
+		},
+		"addSesiones": function(msnObj){
+			//Clases.update({_id:idP},{$set:{like:cont}});
+			Clases.insert(msnObj);
+			///var ids=Clases.find({'cursId':ida},{sort:{ fecha: -1 }, limit: 1}).fetch();
+		    //	console.log(ids);
+			// primero insertamos
+			// ida es el ID del del users
+			// id  es el ID del curso
+			// msnObj es el ID  
+			// var es que el que optiene los el array de clasesId
+			
+			//var ids=Clases.findOne({'cursId':ida},{sort:{ fecha: -1 }, limit: 1}).fetch();
+			
+			//var ids=Clases.find({'clasesId':ida}).fetch();
+			//Cursos.update({_id:id},{$push:{clasesId:ids}});
+			// label: "Tags",
+			//Cursos.update({_id:id},{clasesId:[{ids}]});
+			
+			//Cursos.update({_id:"xnYwCnXbninoxE8ms"}, {$set: {clasesId:p}});
+
+			return true;
+		},
+		"addUsuario":function(id){
+			//console.log(id);	
+			Meteor.users.update(id,{ $set: {'profile.estado':true }});
+		},
+		"delUsuario":function(id){
+			//console.log(id);	
+			Meteor.users.update(id,{ $set: {'profile.estado':false }});
+		},
+		"addEstu":function(id){
+			//console.log(id);	
+			Roles.addUsersToRoles(id,['estudiante'],'estudiante');
+		},
+		"addFaci":function(id){
+			//console.log(id);	
+			Roles.addUsersToRoles(id,['facilitador'],'facilitador');
+		},
+		"updateperfil":function(id,obj){
+			//console.log(id);	
+
+			Meteor.users.update({_id:id},{$set:{
+				'username':obj.username,
+				//'services.password.bcrypt':obj.password,
+				'emails.0.address':obj.email,
+				'profile.nombre':obj.nombre,
+			    'profile.apellido':obj.apellido
+			}});
+		}
+
+
+	});
+	Meteor.publish('listCurso', function() {
+	  return Cursos.find();
+	});
+	Meteor.publish('listCursosMios', function() {
+	  return Cursos.find();
+	});
+
+	Meteor.publish('listPendientes', function() {
+	  
+	  return Meteor.users.find();
+	});
+  
+});
+
+
+Meteor.startup(() => {
 	Meteor.publishComposite("getMSN",function(idUs,idMe){
 		return {
 			find(){
