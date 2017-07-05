1009af1a7a4c3c092c84387df13e97df251089b7
Humberto <humberto1234shakha@hotmail.es>
2017-07-05 23:05:14 +0100

    aplicando roles

--- client/estudiante/listfaci.js
+++ client/estudiante/listfaci.js
@@ -1,6 +1,6 @@
 Template.listface.helpers({
 	listfacilitadores: function(){
 		//console.log(Meteor.users.find({'roles.facilitador':'facilitador'}).fetch());
-		return Meteor.users.find({'roles.facilitador':'facilitador'}).fetch();
+		return Meteor.users.find({'roles.facilitador':'administrador'}).fetch();
 	}
 });
