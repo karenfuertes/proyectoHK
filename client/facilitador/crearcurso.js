ffc481b6e3be412ae82d37aeb8c55f7afcd1a84e
Humberto <humberto1234shakha@hotmail.es>
2017-07-05 12:29:55 +0100

    añadiendo cursos

--- client/facilitador/crearcurso.js
+++ client/facilitador/crearcurso.js
@@ -1,21 +1,45 @@
 Template.crearcurso.events({	
 	"submit form": function(e){
-		e.preventDefault();
-		obj ={
-			nombre:e.target.nombre.value,
-			detalle:e.target.detalle.value,
-			inicio:e.target.inicio.value,
-			fin:e.target.fin.value,	
-			userId : Accounts.user()._id
+		// esta parte hay q llevar al servidor y sera parte del aldo
+		var fechaincribir=e.target.inicio.value;
+		var fechafin=e.target.fin.value;
+
+		var  inscribir= moment(fechaincribir).format("YYYY.MM.DD");
+		var  fin= moment(fechafin).format("YYYY.MM.DD");
+		// fecha actual de la maquina
+		var fechaactual = new Date();
+		var  actual= moment(fechaactual).format("YYYY.MM.DD");
+
+		// > mayor
+		// < menor
+		if(inscribir<=actual){
+			alert("Las Fechas que pusiste estan pasados intente de nuevo");
+			return false;
 		}
-		Meteor.call("addCourse",obj);
-		e.target.nombre.value="";
-		e.target.detalle.value="";
-		e.target.inicio.value="";
-		e.target.fin.value="";	
-		return false;
-	}	
-	
+		else{
+			if(inscribir>fin){
+				alert("La fecha de inicio no puede ser mayor a la fecha del fin");
+				return false;
+			}
+			else{
+				e.preventDefault();
+				obj ={
+					nombre:e.target.nombre.value,
+					detalle:e.target.detalle.value,
+					inicio:e.target.inicio.value,
+					fin:e.target.fin.value,	
+					userId : Accounts.user()._id,
+					
+				}
+				Meteor.call("addCourse",obj);
+				e.target.nombre.value="";
+				e.target.detalle.value="";
+				e.target.inicio.value="";
+				e.target.fin.value="";	
+				return false;
+			}
+		}
+	},
 });
 Template.crearcurso.helpers({
 	username:function(){
