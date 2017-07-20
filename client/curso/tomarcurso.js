8176932c41277f6dbb7e547ed39adbd31d370613
Humberto <humberto1234shakha@hotmail.es>
2017-07-19 21:12:21 -0400

    detalle de materias

--- client/curso/tomarcurso.js
+++ client/curso/tomarcurso.js
@@ -1,3 +1,27 @@
+Template.tomarcurso.onRendered(function(){
+
+	$(document).ready(function(){
+		$(".play-trailer").click(function(){
+			toggleVideo('show');
+			$(".moviecard").addClass("movie-view-trailer");
+		});
+
+		$(".back-btn").click(function(){
+			$(".moviecard").removeClass("movie-view-trailer");
+			toggleVideo('hide');
+		});	
+	});
+
+	function toggleVideo(state) {
+	    // if state == 'hide', hide. Else: show video
+	    var div = document.getElementById("youvideo");
+	    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+	    div.style.display = state == 'hide' ? 'none' : '';
+	    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+	    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
+	}       
+});
+
 
 Template.tomarcurso.helpers({
 	tomarcurso:()=>{
@@ -48,20 +72,25 @@ Template.tomarcurso.helpers({
 });
 
 Template.tomarcurso.helpers({	
+    /*videos(){
+		return Videos.findOne(this.archivo);
+	},
+	imagess() {
+		var res=Cursos.findOne({_id:this.cursId}).imgId;
+		if (res==undefined) {
+			console.log("esta vacio");
+		}
+   		 return Images.findOne(res);
+	},*/
+	readyCu:function(){
+		return FlowRouter.subsReady("listaCursos");
+	},
     clases() {
         return Clases.find();
     },
     clasesCurso() {
         return Cursos.findOne(this.id);
-    },
-    preguntas(){
-    	return Preguntas.find();
-    },
-    autorPreguntas(){
-    	return Meteor.users.findOne(this.idUsuario);
     }
-
-
 
 })
 
