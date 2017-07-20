ae14a14492f16245c6706200aa620d3a074254e4
Karen <karen_fuertes_2013@hotmail.com>
2017-07-20 02:32:00 -0400

    Proyecto mejorado

--- client/usuarios/listfaci.js
+++ client/usuarios/listfaci.js
@@ -1,4 +1,7 @@
-Template.listface.helpers({
+Template.mostrarfaci.helpers({
+	imagen() {
+    	return Images.findOne(this.profile.image);
+  	},
 	listfacilitadores: function(){
 		//console.log(Meteor.users.find({'roles.facilitador':'facilitador'}).fetch());
 		return Meteor.users.find({'roles.facilitador':'facilitador'}).fetch();
